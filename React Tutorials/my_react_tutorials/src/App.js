69bce878ee189e621188acd093ca8de280182a30
Ayon Chatterjee <chatterjeeayon542@gmail.com>
2025-07-16 13:38:44 +0530

    Adding the first lesson of 'React Render HTML'

---
+++ React Tutorials/my_react_tutorials/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John</td>
+        </tr>
+        <tr>
+          <td>Elsa</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default App;
