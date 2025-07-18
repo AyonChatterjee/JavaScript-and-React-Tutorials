c57ab6b6020d910100c239c8722986a7e9e844a4
Ayon Chatterjee <chatterjeeayon542@gmail.com>
2025-07-18 18:35:00 +0530

    React Router

--- React Tutorials/my_react_tutorials/src/App.js
+++ React Tutorials/my_react_tutorials/src/App.js
@@ -2,13 +2,29 @@ import React from "react";
 import Goal from "./ConditionRendering";
 import Garage from "./ReactList";
 import Myform from "./Forms";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Pages/Layout";
+import Home from "./Pages/Home";
+import Blogs from "./Pages/Blogs";
+import Contact from "./Pages/Contact";
+import NoPage from "./Pages/NoPage";
+
 
 function App() {
-  return (   
+  return (
     <div>
-      <Goal isGoal = {true}/>
-       <Garage />
-       <Myform />
+      <BrowserRouter>
+        <Goal isGoal={true} />
+        <Garage />
+        <Myform />
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
 
   );
