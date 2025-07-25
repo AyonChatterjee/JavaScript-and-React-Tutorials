5c26a645fda6b99e9db7cfc5a85b339739ff7171
Ayon Chatterjee <chatterjeeayon542@gmail.com>
2025-07-25 23:38:00 +0530

    Without using useCallback

---
+++ React Tutorials/my_react_tutorials/src/useCallback.jsx
@@ -0,0 +1,22 @@
+import React , {useState} from "react" ;
+
+function App() {
+    const [count , setCount] = useState(0) ;
+
+    const handleClick = () => {
+        console.log("Clicked from child!");
+    } ;
+
+    return (
+        <>
+        <h1>Count: {count}</h1>
+        <button onClick={() => setCount(count + 1)}>Increment Count</button> 
+        <ChildComponent onClick={handleClick} />       
+        </>
+    ) ;
+}
+
+const ChildComponent = React.memo(({ onClick }) =>{
+    console.log("Child rendered") ;
+    return <button onClick={onClick}>Child Button</button>
+});
