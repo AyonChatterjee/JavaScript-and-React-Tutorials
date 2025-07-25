2daff1bbe0747b5c495af414a820bfea0f8aae4f
Ayon Chatterjee <chatterjeeayon542@gmail.com>
2025-07-25 13:39:41 +0530

    Tracking State changes using useRef

--- React Tutorials/my_react_tutorials/src/UseRef.jsx
+++ React Tutorials/my_react_tutorials/src/UseRef.jsx
@@ -1,26 +1,27 @@
-import { useState , useEffect , useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import React from "react";
 
 
 function Application() {
-    const [inputValue , setInputValue] = useState("") ;
-    const count = useRef(0) ;
-
+    const [inputValue, setInputValue] = useState("");
+    const previousInputValue = useRef("");
 
     useEffect(() => {
-        count.current = count.current + 1 ;
-    }) ;
+        previousInputValue.current = inputValue;
+    }, [inputValue]);
 
     return (
         <>
-        <input 
-        type="text" 
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)} />
 
-        <h2>Render Count : {count.current}</h2>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value * 2)} />
+
+            <h2>Current Value : {inputValue}</h2>
+            <h2>Previous Value : {previousInputValue.current}</h2>
+
         </>
-    ) ;
+    );
 }
-
 export default Application;
